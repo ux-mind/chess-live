d51c39abf1ed47ea0045659316fd752496f27715
Pavel Surkov <surkovv.pavel@gmail.com>
2023-02-09 20:49:42 +0700

    adds tournaments data and PGN-viewer cdn

--- src/js/pages/Tournaments/index.jsx
+++ src/js/pages/Tournaments/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import tournaments from '../../data/tournaments';
 
 const Tournaments = () => {
-	return <main className="main tournaments"></main>;
+	// Tournament link does not refer to other route
+	// Open a new tab instead
+	// !NEED TO STORE TOURNAMENTS TABS
+
+	const [tabs, setTabs] = useState([]);
+
+	return (
+		<main className="main tournaments">
+			<div className="container">
+				<div className="tournaments-wrapper">
+					<div className="tournaments-tabs"></div>
+					<div className="tournaments-block"></div>
+				</div>
+			</div>
+		</main>
+	);
 };
+
+{
+	/* <ct-pgn-viewer has-url="true">
+		https://pgnfiles.fide.com/almaty2022/open/blitz/games.pgn
+	</ct-pgn-viewer> */
+}
 
 export default Tournaments;
