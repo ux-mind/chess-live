f1470885a3815935a71c7f4cd082e1d9cc896119
Pavel Surkov <surkovv.pavel@gmail.com>
2023-03-17 22:47:14 +0700

    adds analysis

--- src/js/hooks/useAnalysis.jsx
+++ src/js/hooks/useAnalysis.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const useAnalysis = (fen, multiPv = 1, variant = 'standard') => {
+const useAnalysis = (fen, multiPv = 1, variant = 'standard', listenValue = []) => {
 	const [data, setData] = useState(null);
 	const [error, setError] = useState(null);
 
 	const ANALYSIS_URL = `https://lichess.org/api/cloud-eval`;
 
 	useEffect(() => {
+		if (!fen) {
+			return;
+		}
+
 		const query = async () => {
 			try {
 				const json = await axios.get(ANALYSIS_URL, {
@@ -27,7 +31,7 @@ const useAnalysis = (fen, multiPv = 1, variant = 'standard') => {
 		};
 
 		query();
-	}, []);
+	}, listenValue);
 
 	return { data, error };
 };
--- src/js/pages/Tournaments/Game/Game.jsx
+++ src/js/pages/Tournaments/Game/Game.jsx
@@ -1,53 +1,11 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { convertToFEN } from 'pgn-to-fen';
 import Standings from '../TournamentsContent/Standings/Standings';
 import useAnalysis from '../../../hooks/useAnalysis';
-import { convertToFEN } from 'pgn-to-fen';
 import { parsePgn, startingPosition } from 'chessops/pgn';
 import { parseSan } from 'chessops/san';
 import { makeFen } from 'chessops/fen';
-
-// test for pgn convert
-// const fen = convertToFEN(`
-// 				1. d4 {[%clk 00:03:02]} {[%emt 00:00:02]} Nf6 {[%clk 00:03:03]} 2. Nf3 {[%clk
-// 				00:03:03]} {[%emt 00:00:01]} d5 {[%clk 00:03:04]} 3. Bf4 {[%clk 00:03:04]} Bf5
-// 				{[%clk 00:03:05]} {[%emt 00:00:01]} 4. e3 {[%clk 00:03:01]} {[%emt 00:00:04]}
-// 				e6 {[%clk 00:03:06]} {[%emt 00:00:01]} 5. c4 {[%clk 00:02:52]} {[%emt
-// 				00:00:12]} Bxb1 {[%clk 00:03:06]} {[%emt 00:00:02]} 6. Rxb1 {[%clk 00:02:27]}
-// 				{[%emt 00:00:27]} Bb4+ {[%clk 00:03:07]} {[%emt 00:00:02]} 7. Ke2 {[%clk
-// 				00:02:28]} dxc4 {[%clk 00:02:40]} {[%emt 00:00:28]} 8. Qa4+ {[%clk 00:02:22]}
-// 				{[%emt 00:00:07]} Nc6 {[%clk 00:02:41]} {[%emt 00:00:01]} 9. Ne5 {[%clk
-// 				00:02:16]} {[%emt 00:00:08]} a5 {[%clk 00:01:08]} {[%emt 00:01:35]} 10. Nxc6
-// 				{[%clk 00:02:07]} {[%emt 00:00:12]} Qd7 {[%clk 00:00:57]} {[%emt 00:00:12]} 11.
-// 				a3 {[%clk 00:02:03]} {[%emt 00:00:06]} Bd6 {[%clk 00:00:50]} {[%emt 00:00:07]}
-// 				12. Qxc4 {[%clk 00:01:24]} {[%emt 00:00:42]} Bxf4 {[%clk 00:00:26]} {[%emt
-// 				00:00:26]} 13. exf4 {[%clk 00:01:19]} {[%emt 00:00:07]} bxc6 {[%clk 00:00:27]}
-// 				{[%emt 00:00:01]} 14. g3 {[%clk 00:01:12]} {[%emt 00:00:08]} O-O {[%clk
-// 				00:00:22]} {[%emt 00:00:07]} 15. Bg2 {[%clk 00:01:14]} Nd5 {[%clk 00:00:23]}
-// 				{[%emt 00:00:01]} 16. Rhc1 {[%clk 00:01:08]} {[%emt 00:00:07]} Nb6 {[%clk
-// 				00:00:16]} {[%emt 00:00:10]} 17. Qxc6 {[%clk 00:00:54]} {[%emt 00:00:17]} Qxd4
-// 				{[%clk 00:00:14]} {[%emt 00:00:04]} 18. Qc3 {[%clk 00:00:35]} {[%emt 00:00:20]}
-// 				Qxc3 {[%clk 00:00:13]} {[%emt 00:00:03]} 19. Rxc3 {[%clk 00:00:36]} {[%emt
-// 				00:00:01]} Ra7 {[%clk 00:00:14]} {[%emt 00:00:01]} 20. Rbc1 {[%clk 00:00:31]}
-// 				{[%emt 00:00:07]} Rc8 {[%clk 00:00:15]} 21. Bc6 {[%clk 00:00:31]} {[%emt
-// 				00:00:02]} Kf8 {[%clk 00:00:14]} {[%emt 00:00:02]} 22. a4 {[%clk 00:00:19]}
-// 				{[%emt 00:00:14]} Ke7 {[%clk 00:00:14]} {[%emt 00:00:02]} 23. b3 {[%clk
-// 				00:00:14]} {[%emt 00:00:07]} Nd5 {[%clk 00:00:10]} {[%emt 00:00:06]} 24. Bxd5
-// 				{[%clk 00:00:12]} {[%emt 00:00:04]} exd5 {[%clk 00:00:11]} 25. Rc6 {[%clk
-// 				00:00:09]} {[%emt 00:00:06]} Kd7 {[%clk 00:00:09]} {[%emt 00:00:04]} 26. R6c3
-// 				{[%clk 00:00:07]} {[%emt 00:00:04]} c6 {[%clk 00:00:08]} {[%emt 00:00:02]} 27.
-// 				Rd1 {[%clk 00:00:05]} {[%emt 00:00:04]} Rb7 {[%clk 00:00:08]} {[%emt 00:00:02]}
-// 				28. Re3 {[%clk 00:00:04]} {[%emt 00:00:02]} Re8 {[%clk 00:00:09]} {[%emt
-// 				00:00:02]} 29. Rxe8 Kxe8 30. Rd3 Kd7 {[%clk 00:00:10]} {[%emt 00:00:02]} 31.
-// 				Kd2 {[%clk 00:00:05]} {[%emt 00:00:03]} Kd6 {[%clk 00:00:12]} 32. Kc3 {[%clk
-// 				00:00:04]} {[%emt 00:00:03]} c5 {[%clk 00:00:13]} 33. Kc2 {[%clk 00:00:03]}
-// 				{[%emt 00:00:02]} d4 {[%clk 00:00:13]} 34. Rd1 {[%clk 00:00:03]} {[%emt
-// 				00:00:03]} Re7 {[%clk 00:00:14]} {[%emt 00:00:01]} 35. Kd3 {[%clk 00:00:04]}
-// 				Kd5 {[%clk 00:00:15]} {[%emt 00:00:01]} 36. Rc1 {[%clk 00:00:06]} Rb7 {[%clk
-// 				00:00:16]} 37. Rb1 {[%clk 00:00:07]} Rxb3+ {[%clk 00:00:13]} {[%emt 00:00:05]}
-// 				38. Rxb3 {[%clk 00:00:07]} {[%emt 00:00:01]} c4+ {[%clk 00:00:15]} 39. Kc2
-// 				{[%clk 00:00:08]} cxb3+ {[%clk 00:00:16]} {[%emt 00:00:01]} 40. Kxb3 {[%clk
-// 				00:00:10]} f5 {[%clk 00:00:14]} {[%emt 00:00:03]} 0-1)`);
 
 function calculateBoardHeight(board) {
 	const width = board.offsetWidth;
@@ -56,6 +14,9 @@ function calculateBoardHeight(board) {
 }
 
 const Game = () => {
+	const [currentMove, setCurrentMove] = useState(1);
+	const [fen, setFen] = useState([]);
+
 	const play = () => {
 		const pgn = `1. d4 {[%clk 00:03:02]} {[%emt 00:00:02]} Nf6 {[%clk 00:03:03]} 2. Nf3 {[%clk
 			00:03:03]} {[%emt 00:00:01]} d5 {[%clk 00:03:04]} 3. Bf4 {[%clk 00:03:04]} Bf5
@@ -112,11 +73,12 @@ const Game = () => {
 
 				const fen = makeFen(pos.toSetup());
 
-				console.log(fen);
 				fenMoves.push(fen);
 			}
 
 			console.log(fenMoves);
+
+			setFen(fenMoves);
 
 			// Create a currentMove: number state and change it when user clicks on move btn
 			// Create a fenMoves: number[] state with fen of each move
@@ -125,11 +87,67 @@ const Game = () => {
 		}
 	};
 
-	const navigate = () => {};
+	// For move change
+	useEffect(() => {
+		play();
 
-	// const { data, error } = useAnalysis(fen, 3);
+		window.addEventListener('load', () => {
+			const movesContainer = document.querySelector('.ct-move-list-container');
 
-	// console.log(data);
+			const backBtn = document.querySelector('button[data-nav-cmd="back"]');
+			const forwardBtn = document.querySelector('button[data-nav-cmd="forward"]');
+
+			if (movesContainer) {
+				movesContainer.addEventListener('click', (e) => {
+					const target = e.target;
+					const moveEl = target.closest('.ct-move');
+
+					if (moveEl) {
+						const moveNumber = moveEl.dataset.moveId - 2;
+
+						setCurrentMove(moveNumber);
+					}
+				});
+			}
+
+			if (backBtn && forwardBtn) {
+				backBtn.addEventListener('click', () => setCurrentMove((prev) => prev - 1));
+				forwardBtn.addEventListener('click', () => setCurrentMove((prev) => prev + 1));
+			}
+		});
+
+		return () =>
+			window.removeEventListener('load', () => {
+				const movesContainer = document.querySelector('.ct-move-list-container');
+
+				const backBtn = document.querySelector('button[data-nav-cmd="back"]');
+				const forwardBtn = document.querySelector('button[data-nav-cmd="forward"]');
+
+				if (movesContainer) {
+					movesContainer.addEventListener('click', (e) => {
+						const target = e.target;
+						const moveEl = target.closest('.ct-move');
+
+						if (moveEl) {
+							const moveNumber = moveEl.dataset.moveId - 2;
+
+							setCurrentMove(moveNumber);
+						}
+					});
+				}
+
+				if (backBtn && forwardBtn) {
+					backBtn.addEventListener('click', () => setCurrentMove((prev) => prev - 1));
+					forwardBtn.addEventListener('click', () => setCurrentMove((prev) => prev + 1));
+				}
+			});
+	}, []);
+
+	const { data, error } = useAnalysis(fen[currentMove - 1], 5, 'standard', [fen, currentMove]);
+
+	console.log(fen[currentMove - 1]);
+
+	console.log(data);
 
 	useEffect(() => {
 		const board = document.querySelector('chess-board');
